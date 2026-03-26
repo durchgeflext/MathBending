71c2dab90fa2bec428991f911833d22589a69e30
Felix <felix.wedler@t-online.de>
2026-03-26 13:08:50 +0100

    Added all bitshift functions for unsigned integers

--- include/MathBending/functions/BitShifts.hpp
+++ include/MathBending/functions/BitShifts.hpp
@@ -5,25 +5,159 @@
 #include "MathBending/concepts/ScalarTypes.hpp"
 #include "MathBending/util/Integer128.hpp"
 
-template<U_INT_TYPE T>
-static constexpr uint128_t bit_shift_l(const uint128_t word, const size_t shift) {
-    if (sizeof(T) == sizeof(word)) return word << shift;
-    uint128_t result = 0;
-    const T* parts = reinterpret_cast<const T*>(&word);
-    T* resParts = reinterpret_cast<T*>(&result);
-    for (size_t i = 0; i < sizeof(word)/sizeof(T); i++) {
+// ==============================================================================
+//            8 bit word segmented shift
+// ==============================================================================
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint8_t segmnt_bit_shift_l(const uint8_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) == 1, "The segment size cannot be greater than the word size");
+
+    return word << shift;
+}
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint8_t segmnt_bit_shift_r(const uint8_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) == 1, "The segment size cannot be greater than the word size");
+
+    return word >> shift;
+}
+
+
+
+
+// ==============================================================================
+//            16 bit word segmented shift
+// ==============================================================================
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint16_t segmnt_bit_shift_l(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 2, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word << shift;
+    uint16_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
         resParts[i] = parts[i] << shift;
     }
     return result;
 }
 
-template<U_INT_TYPE T>
-static constexpr uint128_t bit_shift_r(const uint128_t word, const size_t shift) {
-    if (sizeof(T) == sizeof(word)) return word >> shift;
+template<U_INT_TYPE sgmnt>
+static constexpr uint16_t segmnt_bit_shift_r(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 2, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word >> shift;
+    uint16_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] >> shift;
+    }
+    return result;
+}
+
+
+
+
+// ==============================================================================
+//            32 bit word segmented shift
+// ==============================================================================
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint32_t segmnt_bit_shift_l(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 4, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word << shift;
+    uint32_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] << shift;
+    }
+    return result;
+}
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint32_t segmnt_bit_shift_r(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 4, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word >> shift;
+    uint32_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] >> shift;
+    }
+    return result;
+}
+
+
+
+
+// ==============================================================================
+//            64 bit word segmented shift
+// ==============================================================================
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint64_t segmnt_bit_shift_l(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 8, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word << shift;
+    uint64_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] << shift;
+    }
+    return result;
+}
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint64_t segmnt_bit_shift_r(const uint64_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 8, "The segment size cannot be greater than the word size");
+
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word >> shift;
+    uint64_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] >> shift;
+    }
+    return result;
+}
+
+
+
+
+// ==============================================================================
+//            128 bit word segmented shift
+// ==============================================================================
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint128_t segmnt_bit_shift_l(const uint128_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 16, "The segment size cannot be greater than the word size");
+    
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word << shift;
     uint128_t result = 0;
-    const T* parts = reinterpret_cast<const T*>(&word);
-    T* resParts = reinterpret_cast<T*>(&result);
-    for (size_t i = 0; i < sizeof(word)/sizeof(T); i++) {
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
+        resParts[i] = parts[i] << shift;
+    }
+    return result;
+}
+
+template<U_INT_TYPE sgmnt>
+static constexpr uint128_t segmnt_bit_shift_r(const uint128_t word, const size_t shift) {
+    static_assert(sizeof(sgmnt) <= 16, "The segment size cannot be greater than the word size");
+    
+    if constexpr (sizeof(sgmnt) == sizeof(word)) return word >> shift;
+    uint128_t result = 0;
+    const auto* parts = reinterpret_cast<const sgmnt*>(&word);
+    auto* resParts = reinterpret_cast<sgmnt*>(&result);
+    for (size_t i = 0; i < sizeof(word)/sizeof(sgmnt); i++) {
         resParts[i] = parts[i] >> shift;
     }
     return result;
--- test/bitShift_test.cpp
+++ test/bitShift_test.cpp
@@ -6,9 +6,9 @@ int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
     uint128_t test1 = 0;
     uint128_t test2 = 0;
 
-    test1 = bit_shift_l<uint128_t>(test1, 0);
+    test1 = segmnt_bit_shift_l<uint128_t>(test1, 0);
     if (test1 != 0) return 1;
-    test2 = bit_shift_r<uint128_t>(test2, 0);
+    test2 = segmnt_bit_shift_r<uint128_t>(test2, 0);
     if (test2 != 0) return 2;
     return 0;
 }
