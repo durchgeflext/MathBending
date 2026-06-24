00f8a9a44d6b734b283ee9a7f98a81fa0852b136
Felix <felix.wedler@t-online.de>
2026-06-24 15:31:31 +0200

    Started implementing tags for segmented bit_shifts

--- include/MathBending/functions/BitShifts.hpp
+++ include/MathBending/functions/BitShifts.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 #include <cstddef>
+#include <emmintrin.h>
+#include <cstring>
 
 #include "MathBending/concepts/ScalarTypes.hpp"
 #include "MathBending/util/Integer128.hpp"
-#include "MathBending/platform.hpp"
+#include "MathBending/platform/platform.hpp"
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift) {
+    word_t result = 0;
+    for (size_t i = 0; i < sizeof(word_t) / sizeof(sgmnt_t); i++) {
+        sgmnt_t part;
+        std::memcpy(&part, reinterpret_cast<char *>(&word) + i * sizeof(sgmnt_t), sizeof(sgmnt_t));
+        part <<= shift;
+        std::memcpy(reinterpret_cast<char*>(&word) + i * sizeof(sgmnt_t), sizeof(sgmnt_t));
+    }
+    return result;
+}
 
 template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
 static constexpr word_t sgmnt_bit_shift_l(const word_t word, const size_t shift) {
@@ -12,20 +26,18 @@ static constexpr word_t sgmnt_bit_shift_l(const word_t word, const size_t shift)
 
     if (sizeof(sgmnt_t) == sizeof(word_t)) return word << shift;
 
+    word_t result = 0;
 #ifdef SSE2_SET
+    //TODO: use tagging for overloading this
     if constexpr (sizeof(word_t) == 16 && sizeof(sgmnt_t) == 4) {
-        //TODO: I hate platform specific stuff...
-        //__m128i a =
+        const __m128i a = _mm_loadu_si128(static_cast<const __m128i *>(&word));
+        const __m128i r = _mm_slli_epi32(a, static_cast<int>(shift));
+        _mm_storeu_si128(static_cast<__m128i *>(&result), r);
     }
 #else
-    word_t result = 0;
-    const auto* parts = reinterpret_cast<const sgmnt_t*>(&word);
-    auto* resParts = reinterpret_cast<sgmnt_t*>(&result);
-    for (size_t i = 0; i < sizeof(word_t) / sizeof(sgmnt_t); i++) {
-        resParts[i] = parts[i] << shift;
-    }
-    return result;
+    sgmnt_bit_shift_l_impl<word_t, sgmnt_t>(word, shift);
 #endif
+    return result;
 }
 
 template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
---
+++ include/MathBending/platform/sse2.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+struct sse2_epi8_tag {};
+struct sse2_epi16_tag {};
+struct sse2_epi32_tag {};
+struct sse2_epi64_tag {};
+struct sse2_epi128_tag {};
