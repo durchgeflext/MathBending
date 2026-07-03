6fbc969a1704cb957612c4fa73a76572ed912e97
Felix <felix.wedler@t-online.de>
2026-07-03 17:33:35 +0200

    Implemented vectorized segmented shifts

--- include/MathBending/functions/BitShifts.hpp
+++ include/MathBending/functions/BitShifts.hpp
@@ -2,21 +2,58 @@
 
 #include <cstddef>
 #include <emmintrin.h>
-#include <cstring>
-
 #include "MathBending/concepts/ScalarTypes.hpp"
+#include "MathBending/platform/sse2.hpp"
 #include "MathBending/util/Integer128.hpp"
-#include "MathBending/platform/platform.hpp"
 
 template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
-static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift) {
-    word_t result = 0;
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift, scalar_tag) {
+    sgmnt_t sgmnt_mask = static_cast<sgmnt_t>(~0) << shift;
+    word_t mask = 0;
     for (size_t i = 0; i < sizeof(word_t) / sizeof(sgmnt_t); i++) {
-        sgmnt_t part;
-        std::memcpy(&part, reinterpret_cast<char *>(&word) + i * sizeof(sgmnt_t), sizeof(sgmnt_t));
-        part <<= shift;
-        std::memcpy(reinterpret_cast<char*>(&word) + i * sizeof(sgmnt_t), sizeof(sgmnt_t));
+        mask <<= sizeof(sgmnt_t) * 8;
+        mask |= sgmnt_mask;
     }
+    return (word << shift) & mask;
+}
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift, sse2_epi8_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(&word);
+    const __m128i mask = _mm_set1_epi8(static_cast<char>(0xff << shift));
+    const __m128i r = _mm_and_si128(_mm_slli_epi16(a, static_cast<int>(shift)), mask);
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift, sse2_epi16_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<__m128i *>(&word));
+    const __m128i r = _mm_slli_epi16(a,static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift, sse2_epi32_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<__m128i *>(&word));
+    const __m128i r = _mm_slli_epi32(a,static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_l_impl(const word_t word, const size_t shift, sse2_epi64_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<const __m128i *>(&word));
+    const __m128i r = _mm_slli_epi64(a, static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i *>(&result), r);
     return result;
 }
 
@@ -26,17 +63,54 @@ static constexpr word_t sgmnt_bit_shift_l(const word_t word, const size_t shift)
 
     if (sizeof(sgmnt_t) == sizeof(word_t)) return word << shift;
 
-    word_t result = 0;
-#ifdef SSE2_SET
-    //TODO: use tagging for overloading this
-    if constexpr (sizeof(word_t) == 16 && sizeof(sgmnt_t) == 4) {
-        const __m128i a = _mm_loadu_si128(static_cast<const __m128i *>(&word));
-        const __m128i r = _mm_slli_epi32(a, static_cast<int>(shift));
-        _mm_storeu_si128(static_cast<__m128i *>(&result), r);
+    return sgmnt_bit_shift_l_impl<word_t, sgmnt_t>(word, shift, shift_tag<word_t, sgmnt_t>());
+}
+
+template <U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_r_impl(const word_t word, const size_t shift, scalar_tag) {
+    sgmnt_t sgmnt_mask = static_cast<sgmnt_t>(~0) >> shift;
+    word_t mask= 0;
+    for (size_t i = 0; i < sizeof(word_t) / sizeof(sgmnt_t); i++) {
+        mask <<= sizeof(sgmnt_t) * 8;
+        mask |= sgmnt_mask;
     }
-#else
-    sgmnt_bit_shift_l_impl<word_t, sgmnt_t>(word, shift);
-#endif
+    return (word >> shift) & mask;
+}
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_r_impl(const word_t word, const size_t shift, sse2_epi8_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(&word);
+    const __m128i mask = _mm_set1_epi8(static_cast<char>(0xff >> shift));
+    const __m128i r = _mm_and_si128(_mm_srli_epi16(a, static_cast<int>(shift)), mask);
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_r_impl(const word_t word, const size_t shift, sse2_epi16_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<__m128i *>(&word));
+    const __m128i r = _mm_srli_epi16(a,static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_r_impl(const word_t word, const size_t shift, sse2_epi32_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<__m128i *>(&word));
+    const __m128i r = _mm_srli_epi32(a,static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
+    return result;
+}
+
+template<U_INT_TYPE word_t, U_INT_TYPE sgmnt_t>
+static constexpr word_t sgmnt_bit_shift_r_impl(const word_t word, const size_t shift, sse2_epi64_tag) {
+    word_t result = 0;
+    const __m128i a = _mm_loadu_si128(static_cast<__m128i *>(&word));
+    const __m128i r = _mm_srli_epi64(a,static_cast<int>(shift));
+    _mm_storeu_si128(static_cast<__m128i*>(&result), r);
     return result;
 }
 
@@ -45,11 +119,6 @@ static constexpr word_t sgmnt_bit_shift_r(const word_t word, const size_t shift)
     static_assert(sizeof(sgmnt_t) <= sizeof(word_t), "The segment size cannot be greater than the word size");
 
     if (sizeof(sgmnt_t) == sizeof(word_t)) return word >> shift;
-    word_t result = 0;
-    const auto* parts = reinterpret_cast<const sgmnt_t*>(&word);
-    auto* resParts = reinterpret_cast<sgmnt_t*>(&result);
-    for (size_t i = 0; i < sizeof(word_t) / sizeof(sgmnt_t); i++) {
-        resParts[i] = parts[i] >> shift;
-    }
-    return result;
+
+    return sgmnt_bit_shift_r_impl<word_t,sgmnt_t>(word, shift, shift_tag<word_t, sgmnt_t>());
 }
--- include/MathBending/platform/sse2.hpp
+++ include/MathBending/platform/sse2.hpp
@@ -1,7 +1,25 @@
 #pragma once
+#include "MathBending/concepts/ScalarTypes.hpp"
+#include "MathBending/platform/platform.hpp"
+
+struct scalar_tag {};
 
 struct sse2_epi8_tag {};
 struct sse2_epi16_tag {};
 struct sse2_epi32_tag {};
 struct sse2_epi64_tag {};
 struct sse2_epi128_tag {};
+
+template<U_INT_TYPE word_t, U_INT_TYPE shift_t>
+constexpr auto shift_tag() {
+#ifdef SSE2_SET
+    if constexpr (sizeof(word_t) < 16)  return scalar_tag{};
+    if constexpr (sizeof(shift_t) == 1) return sse2_epi8_tag{};
+    if constexpr (sizeof(shift_t) == 2) return sse2_epi16_tag{};
+    if constexpr (sizeof(shift_t) == 4) return sse2_epi32_tag{};
+    if constexpr (sizeof(shift_t) == 8) return sse2_epi64_tag{};
+    if constexpr (sizeof(shift_t) == 16) return sse2_epi128_tag{};
+#else
+    return scalar_tag{};
+#endif
+}
